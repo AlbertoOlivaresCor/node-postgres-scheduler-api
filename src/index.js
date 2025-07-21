bbfdda009fcae961499968bd7c8520ed938a3470
Alberto Olivares <220054440+AlbertoOlivaresCor@users.noreply.github.com>
2025-07-21 19:02:09 -0400

    Implementado firebase con autenticacion y pruebas de generacion de horarios. Controlando que solo con token se pueda generar el horario y la disponi bilidad de los profesores.

--- src/index.js
+++ src/index.js
@@ -3,9 +3,9 @@ import 'dotenv/config'
 import { PORT } from './config.js'
 
 import profesoresRoutes from './routes/profesores.routes.js'
-//import cursosRoutes from './routes/cursos.routes.js';
-//import salasRoutes from './routes/salas.routes.js';
-//import disponibilidadesRoutes from './routes/disponibilidades.routes.js';
+import cursosRoutes from './routes/cursos.routes.js';
+import salasRoutes from './routes/salas.routes.js';
+import disponibilidadesRoutes from './routes/disponibilidades.routes.js';
 import horarioRoutes from './routes/horario.routes.js';
 import authRoutes from './routes/auth.routes.js';
 import { authenticateFirebase } from './middlewares/firebaseAuth.js';
@@ -14,7 +14,10 @@ const app = express()
 app.use(express.json())
 
 app.use(authRoutes); // Registro y login
-app.use(profesoresRoutes); // Público o protegido según necesidad
-app.use('/horarios', authenticateFirebase, horarioRoutes); // Solo usuarios autenticados pueden acceder a horarios
+app.use(profesoresRoutes); // /profesores
+app.use(cursosRoutes);      // /cursos
+app.use(salasRoutes);       // /salas
+app.use(disponibilidadesRoutes); // /disponibilidades
+app.use('/', authenticateFirebase, horarioRoutes); // Solo usuarios autenticados pueden acceder a horarios
 
 app.listen(PORT, () => console.log(`Servidor en puerto ${PORT}`))
